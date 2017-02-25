00617aa106287c16f72238dc0f5543b2ec8eb5fe
Thuc Le <ledongthuc@gmail.com>
2017-02-25 16:19:34 +0700

    First stone

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/russross/blackfriday"
+)
 
 func main() {
-	fmt.Println("Hello noter")
+	input := "# Header\ncontent\n#HEader\n*a*"
+
+	inputInBytes := []byte(input)
+	extensions := 0
+	extensions |= blackfriday.EXTENSION_NO_INTRA_EMPHASIS
+	extensions |= blackfriday.EXTENSION_TABLES
+	extensions |= blackfriday.EXTENSION_FENCED_CODE
+	extensions |= blackfriday.EXTENSION_AUTOLINK
+	extensions |= blackfriday.EXTENSION_STRIKETHROUGH
+	extensions |= blackfriday.EXTENSION_SPACE_HEADERS
+	htmlFlags := 0
+	htmlFlags |= blackfriday.HTML_USE_XHTML
+	htmlFlags |= blackfriday.HTML_USE_SMARTYPANTS
+	htmlFlags |= blackfriday.HTML_SMARTYPANTS_FRACTIONS
+	htmlFlags |= blackfriday.HTML_SMARTYPANTS_LATEX_DASHES
+
+	render := blackfriday.HtmlRenderer(htmlFlags, "", "")
+	output := blackfriday.Markdown(inputInBytes, render, extensions)
+
+	fmt.Println(string(output))
 }
